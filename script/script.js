d1946d10e738d356fed37408f019bf8345dc110c
Luiz Eduardo <eduardoveltroni@hotmail.com>
2021-03-23 02:28:36 -0300

    Refactoring JavaScript

--- script/script.js
+++ script/script.js
@@ -1,31 +1,23 @@
 // Functions to open and close the modal by clicking on the hamburger menu
 const ModalMenu = {
     open() {
-        document
-            .querySelector('.modal__overlay.menu__modal')
-            .classList.add('active2')
+        document.querySelector('.modal__overlay.menu__modal').classList.add('active2')
 
-            document
-            .querySelector('.hamburguer')
-            .classList.add('remove')
+        document.querySelector('.hamburguer').classList.add('remove')
     },
 
     close() {
-        document
-            .querySelector('.modal__overlay.menu__modal')
-            .classList.remove('active2')
-
-            document
-            .querySelector('.hamburguer')
-            .classList.remove('remove')
+        document.querySelector('.modal__overlay.menu__modal').classList.remove('active2')
+        
+        document.querySelector('.hamburguer').classList.remove('remove')
     }
-
 }
 
-// Scroll slow motion on lick 
+// Object for functions related to the Scroll
 const Scroll = {
-     scrollslowmotion() {
-         // Reaching out
+
+    headerchange() {
+         // Checking if the scroll rolled
         if (window.scrollY !== 0) {
             document.querySelector('.header').classList.add('header__movescroll')
 
@@ -38,38 +30,27 @@ const Scroll = {
             let img = document.querySelector('#img-logo')
             img.setAttribute('src', './assets/img/web/logo.png')
         }
+    },
+
+    // "About" presentation animation
+    animationScroll() {
+        const algo__about = document.querySelectorAll('[data-animation]')
+        const animationClass = 'animate'
+        const heightServices = document.querySelector('.services')
+        const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    
+        algo__about.forEach(function(element) {
+            //Setting height to close the about
+            const sum = (heightServices.offsetTop / 10) * 9
+            if (windowTop > element.offsetTop && window.pageYOffset <sum) {
+                element.classList.add(animationClass)
+            } else {
+                element.classList.remove(animationClass)
+            }
+        })
     }
-
-
 }
 
-document.addEventListener("scroll", Scroll.scrollslowmotion, false)
+document.addEventListener("scroll", Scroll.headerchange, false)
 
-
-// "About" presentation animation
-const algo__about = document.querySelectorAll('[data-animation]')
-const animationClass = 'animate'
-const heightServices = document.querySelector('.services')
-
-function animationScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
-
-    algo__about.forEach(function(element) {
-        //Setting height to close the about
-        const sum = (heightServices.offsetTop / 10) * 9
-        if (windowTop > element.offsetTop && window.pageYOffset <sum) {
-            element.classList.add(animationClass)
-        } else {
-            element.classList.remove(animationClass)
-        }
-    })
-}
-
-animationScroll()
-
-// Check if you have Data-animation on the screen
-if (algo__about.length) {
-    window.addEventListener('scroll', function(){
-        animationScroll()
-})
-}
+window.addEventListener('scroll', () => Scroll.animationScroll())
